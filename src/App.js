46a96e47f598b0285e3ccb35b79308f4bb9c76fa
Loi Tran <datloiboi@gmail.com>
2019-10-27 00:46:33 +0700

    Updae

--- src/App.js
+++ src/App.js
@@ -3,31 +3,29 @@ import "./App.css";
 
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
-
 import "bootstrap/dist/css/bootstrap.min.css";
+
+import Navbar from "./components/Navbar.js";
 
 import CompanyPage from "./pages/CompanyPage";
 import CandidatesPage from "./pages/CandidatesPage";
+import DashboardPage from "./pages/DashboardPage.js";
 import NewCandidatePage from "./pages/NewCandidatePage";
 import EditCandidatePage from "./pages/EditCandidatePage.js";
-import DashboardPage from "./pages/DashboardPage.js";
-import Navbar from "./components/Navbar.js";
 
 export default function App() {
   return (
-    <div className="container">
-      <Router>
-        <Navbar />
-        <Route path="/" exact component={CompanyPage} />
-        <Route path="/company" component={CompanyPage} />
-        <Route path="/candidates/" exact component={CandidatesPage} />
-        <Route path="/candidates/new" component={NewCandidatePage} />
-        <Route path="/dashboard" component={DashboardPage} />
-        <Route
-          path="/candidates/:id/edit"
-          render={props => <EditCandidatePage {...props} />}
-        />
-      </Router>
-    </div>
+    <Router>
+      <Navbar />
+      <Route path="/" exact component={CompanyPage} />
+      <Route path="/company" component={CompanyPage} />
+      <Route path="/candidates/" exact component={CandidatesPage} />
+      <Route path="/candidates/new" component={NewCandidatePage} />
+      <Route path="/dashboard" component={DashboardPage} />
+      <Route
+        path="/candidates/:id/edit"
+        render={props => <EditCandidatePage {...props} />}
+      />
+    </Router>
   );
 }
--- src/pages/CandidatesPage.js
+++ src/pages/CandidatesPage.js
@@ -1,9 +1,10 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 
 import {
   Row,
   Col,
   Card,
+  Spinner,
   ListGroup,
   Container,
   ListGroupItem
@@ -27,83 +28,101 @@ const getImageUrl = candidate => {
   return `https://randomuser.me/api/portraits/${gender}/${num}.jpg`;
 };
 
-export default class CandidatesPage extends Component {
-  state = {
-    candidates: []
-  };
-  async componentDidMount() {
-    const response = await fetch("http://localhost:3001/candidates");
-    const data = await response.json();
-    this.setState({ candidates: data });
-  }
+export default function CandidatesPage(props) {
+  const [candidates, setCandidates] = useState([]);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    const getCandidates = async () => {
+      try {
+        const response = await fetch("http://localhost:3001/candidates");
+        const data = await response.json();
+        setCandidates(data);
+        setLoading(false);
+      } catch (error) {
+        setCandidates(require('../candidates.json'));
+        console.log("Oops");
+      }
+    };
+    getCandidates();
+  }, []);
 
-  onDeleteCandidate = id => {
+  const onDeleteCandidate = id => {
     try {
       const config = { method: "DELETE" };
       fetch(`http://localhost:3001/candidates/${id}`, config);
-      const newCandidates = this.state.candidates.filter(
-        candidate => candidate.id !== id
-      );
-      this.setState({ candidates: newCandidates });
+      const newCandidates = candidates.filter(candidate => candidate.id !== id);
+      setCandidates(newCandidates);
     } catch (error) {
       console.log("Error: ", error);
     }
   };
 
-  render() {
+  if (loading) {
     return (
-      <Container fluid style={{ paddingTop: 100 }}>
+      <Container
+        fluid
+        style={{ paddingTop: 100, height: '100vh' }}
+        className="d-flex justify-content-center align-items-center"
+      >
         <Row>
-          {this.state.candidates.map(candidate => {
-            return (
-              <Col lg="3" key={candidate.id}>
-                <Card>
-                  {/* <Card.Img variant="top" src={getImageUrl(candidate)} /> */}
-                  <Card.Img variant="top" src={candidate.profile_pic_url} />
-                  <Card.Body>
-                    <Card.Title>
-                      {candidate.first_name + " " + candidate.last_name}
-                    </Card.Title>
-                    <Card.Text>{candidate.id}</Card.Text>
-                  </Card.Body>
-                  <ListGroup className="list-group-flush">
-                    <ListGroupItem>
-                      <FontAwesomeIcon icon={faBriefcase} /> {candidate.company}
-                    </ListGroupItem>
-                    <ListGroupItem>
-                      <FontAwesomeIcon icon={faUserMd} /> {candidate.job_title}
-                    </ListGroupItem>
-                    <ListGroupItem>
-                      <FontAwesomeIcon icon={faVenusMars} /> {candidate.gender}
-                    </ListGroupItem>
-                    <ListGroupItem>
-                      <FontAwesomeIcon icon={faMapPin} />{" "}
-                      {candidate.locations && candidate.locations[0].city}
-                    </ListGroupItem>
-                    <ListGroupItem>
-                      <FontAwesomeIcon icon={faMap} />{" "}
-                      {candidate.locations && candidate.country}
-                    </ListGroupItem>
-                    <ListGroupItem>
-                      <FontAwesomeIcon icon={faEnvelope} /> {candidate.email}
-                    </ListGroupItem>
-                  </ListGroup>
-                  <Card.Body>
-                    <Card.Link
-                      onClick={() => this.onDeleteCandidate(candidate.id)}
-                    >
-                      <FontAwesomeIcon icon={faTrash} /> Remove
-                    </Card.Link>
-                    <Card.Link href={`/candidates/${candidate.id}/edit`}>
-                      <FontAwesomeIcon icon={faEdit} /> Edit
-                    </Card.Link>
-                  </Card.Body>
-                </Card>
-              </Col>
-            );
-          })}
+          <Spinner variant="primary" animation="grow" role="status">
+            <span className="sr-only">Loading...</span>
+          </Spinner>
         </Row>
       </Container>
     );
   }
+
+  return (
+    <Container fluid style={{ paddingTop: 100 }}>
+      <Row>
+        {candidates.map(candidate => {
+          return (
+            <Col lg="3" key={candidate.id}>
+              <Card>
+                {/* <Card.Img variant="top" src={getImageUrl(candidate)} /> */}
+                <Card.Img variant="top" src={candidate.profile_pic_url} />
+                <Card.Body>
+                  <Card.Title>
+                    {candidate.first_name + " " + candidate.last_name}
+                  </Card.Title>
+                  <Card.Text>{candidate.id}</Card.Text>
+                </Card.Body>
+                <ListGroup className="list-group-flush">
+                  <ListGroupItem>
+                    <FontAwesomeIcon icon={faBriefcase} /> {candidate.company}
+                  </ListGroupItem>
+                  <ListGroupItem>
+                    <FontAwesomeIcon icon={faUserMd} /> {candidate.job_title}
+                  </ListGroupItem>
+                  <ListGroupItem>
+                    <FontAwesomeIcon icon={faVenusMars} /> {candidate.gender}
+                  </ListGroupItem>
+                  <ListGroupItem>
+                    <FontAwesomeIcon icon={faMapPin} />{" "}
+                    {candidate.locations && candidate.locations[0].city}
+                  </ListGroupItem>
+                  <ListGroupItem>
+                    <FontAwesomeIcon icon={faMap} />{" "}
+                    {candidate.locations && candidate.country}
+                  </ListGroupItem>
+                  <ListGroupItem>
+                    <FontAwesomeIcon icon={faEnvelope} /> {candidate.email}
+                  </ListGroupItem>
+                </ListGroup>
+                <Card.Body>
+                  <Card.Link onClick={() => onDeleteCandidate(candidate.id)}>
+                    <FontAwesomeIcon icon={faTrash} /> Remove
+                  </Card.Link>
+                  <Card.Link href={`/candidates/${candidate.id}/edit`}>
+                    <FontAwesomeIcon icon={faEdit} /> Edit
+                  </Card.Link>
+                </Card.Body>
+              </Card>
+            </Col>
+          );
+        })}
+      </Row>
+    </Container>
+  );
 }
